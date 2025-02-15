f44a9dd8eb9330063d37cdeb8f84e173531cab5f
erick <erigomes05@gmail.com>
2025-02-14 21:30:28 -0300

    first version of menu

--- code/Game.py
+++ code/Game.py
@@ -1,12 +1,13 @@
 import pygame
 
 from code.Menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
@@ -14,8 +15,4 @@ class Game:
             menu.run()
             pass
 
-            # check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # close window
-            #         quit()  # end pygame
+
---
+++ code/const.py
@@ -0,0 +1,13 @@
+# C
+COLOR_YELLOW = (255, 215, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P - COOPERATIVE',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE', 'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
