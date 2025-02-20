d8bea558b84d2a64fb925cd979cc772f35080fa5
erick <erigomes05@gmail.com>
2025-02-19 21:13:24 -0300

    feat: add `Entity`, `EntityFactory` and `Level`

--- code/const.py
+++ code/const.py
@@ -2,8 +2,20 @@
 COLOR_YELLOW = (255, 215, 0)
 COLOR_WHITE = (255, 255, 255)
 
+# E
+ENTITY_SPEED = {
+    'level-um-bg1': 0,
+    'level-um-bg2': 1,
+    'level-um-bg3': 2,
+    'level-um-bg4': 3,
+    'level-um-bg5': 4,
+    'level-um-bg6': 5,
+    'level-um-bg7': 6,
+
+}
+
 # M
-MENU_OPTION = ('NEW GAME 1P - COOPERATIVE',
+MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE', 'EXIT')
